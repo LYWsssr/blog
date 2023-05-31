631f07b5781f2d9698c4b0bbf28320f0c8c435ab
LYWsssr <183856700@qq.com>
2023-05-31 14:08:00 +0800

    更新

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,74 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  lang: 'en-US',
+  title: "LG-note",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '笔记', link: '/Vue/index.md' }
+    ],
+    smoothScroll: true,
+    outlineTitle: 'In hac pagina',
+    logo: 'https://api.iconify.design/logos:android-icon.svg?color=%23e0fffb',
+    sidebar: getSidebar(),
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    }
+  }
+})
+function getSidebar() {
+  return [
+    {
+      text: '前端',
+      items: [
+        {
+          text: 'HTML',
+          items: [
+            { text: 'HTML基础', link: '/HTML/index.md', },
+            { text: 'HTML进阶', link: '/HTML/advanced.md', },
+            { text: 'CSS基础', link: '/CSS/' },
+            { text: 'CSS进阶', link: '/CSS/advanced' },
+            { text: 'Javascript基础', link: '/Javascript/' },
+            { text: 'Javascript进阶', link: '/Javascript/advanced' },
+            { text: '进阶', link: '/Javascript/nightmare' },
+          ]
+        },
+        {
+          text: 'Vue',
+          items: [
+            { text: 'Vue基础', link: '/Vue/' },
+            { text: 'Vue进阶', link: '/Vue/advanced' },
+          ]
+        },
+        {
+          text: '浏览器',
+          items: [
+            { text: '浏览器基础', link: '/Vue/' },
+            { text: '浏览器进阶', link: '/Vue/advanced' },
+            { text: '网络基础', link: '/Network/' },
+            { text: '网络进阶', link: '/Network/advanced' },
+            { text: '安全基础', link: '/Security/' },
+            { text: '安全进阶', link: '/Security/advanced' },
+          ]
+        },
+        {
+          text: '面经',
+          items: [
+            { text: '面经基础', link: '/Experience/' },
+            { text: '面经进阶', link: '/Experience/advanced' },
+          ]
+        },
+      ],
+    },
+
+
+  ]
+}
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "LG note"
+  text: "Blog"
+  tagline: 备忘录
+  image:
+    src: /javascript.gif
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /HTML/index.md
+    - theme: alt
+      text: View on GitHub
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
